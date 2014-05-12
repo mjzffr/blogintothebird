3ceb66c21e749f72e9018a9ad296fa96fc15861a
Maja Frydrychowicz <mjzffr@gmail.com>
2014-05-12 08:33:25 -0400

    First theme test. Custom Makefile.

---
+++ content/test.md
@@ -0,0 +1,9 @@
+Title: Stuff
+Date: 2014-05-11 12:16
+Category: things
+Tags: blah
+Slug: test-slug
+Author: Maja Frydrychowicz
+Summary: test
+
+test blah blah
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Maja Z. Frydrychowicz'
+SITENAME = u'Maja Z. Frydrychowicz'
+GITHUB_URL = 'http://github.com/mjzffr/'
+TWITTER_USERNAME = 'maja_zf'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+# LINKS =  (('Pelican', 'http://getpelican.com/'),
+#           ('Python.org', 'http://python.org/'),
+#           ('Jinja2', 'http://jinja.pocoo.org/'),
+#           ('You can modify those links in your config file', '#'),)
+LINKS = False
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/maja_zf'),)
+
+DEFAULT_PAGINATION = 4
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = "notmyidea"
